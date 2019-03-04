7bcb2b45d9c6733bc6d59b9e4e3ae3d329a58d78
Guillaume BADINIER <gub@softfluent.com>
2019-03-04 12:12:18 +0100

    First commit

---
+++ src/StringCalculator.Kata.Services/StringCalculatorService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator.Kata.Services
+{
+    public class StringCalculatorService
+    {
+        public int Add(string numbers)
+        {
+            CheckErrors(numbers);
+
+            if (numbers.Contains("//") && numbers.Contains("\n"))
+            {
+                var newString = numbers.Replace("//", "\n");
+                var result = newString.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                char delimiter;
+                var isParse = char.TryParse(result[0], out delimiter);
+                if(isParse)
+                {
+                    var allnumbers = string.Join(delimiter.ToString(), result[1]);
+                    return SumStringWithCommas(allnumbers, delimiter);
+                }
+            }
+            else if (numbers.Contains("\n") || numbers.Contains(","))
+            {
+                var numbersWithoutNewLine = numbers.Replace("\n", ",");
+                return SumStringWithCommas(numbersWithoutNewLine, ',');
+            }
+
+            return string.IsNullOrEmpty(numbers) ? 0 : Convert.ToInt32(numbers);
+        }
+
+        private static int SumStringWithCommas(string numbers, char delimiter)
+        {
+            var allnumbers = numbers.Split(delimiter);
+            return allnumbers.Select(x => Convert.ToInt32(x)).Sum();
+        }
+
+        private static void CheckErrors(string numbers)
+        {
+            if (numbers.Contains("\n,"))
+            {
+                throw new ArgumentException("\n, is not authorized");
+            }
+            else
+            {
+                var regex = new Regex(@"-[0-9]");
+                MatchCollection matches = regex.Matches(numbers);
+                if (matches.Count > 0)
+                {
+                    var negativeNumbers = matches.Cast<Match>().Select(x => x.Value);
+                    var result = string.Join(",", negativeNumbers);
+                    throw new ArgumentException($"Negatives are not allowed (result)");
+                }
+            }
+        }
+    }
+}
---
+++ tests/StringCalculator.Kata.Tests/StringCalculatorServiceTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator.Kata.Services;
+
+namespace StringCalculator.Kata.Tests
+{
+    [TestClass]
+    public class StringCalculatorServiceTest
+    {
+        private readonly StringCalculatorService stringCalculatorService;
+
+        public StringCalculatorServiceTest()
+        {
+            this.stringCalculatorService = new StringCalculatorService();
+        }
+
+        [TestMethod]
+        public void should_return_0_when_string_is_empty()
+        {
+            var numbers = string.Empty;
+            var result = stringCalculatorService.Add(numbers);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void should_return_number_when_string_contains_only_one_number()
+        {
+            var numbers = "1";
+            var result = stringCalculatorService.Add(numbers);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void should_return_result_addition_when_string_contains_two_numbers()
+        {
+            var numbers = "1,3";
+            var result = stringCalculatorService.Add(numbers);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void should_return_result_addition_when_string_contains_multiple_numbers()
+        {
+            var numbers = "1,3,5,8,4";
+            var result = stringCalculatorService.Add(numbers);
+            Assert.AreEqual(21, result);
+        }
+
+        [TestMethod]
+        public void should_return_result_addition_when_string_contains_new_lines()
+        {
+            var numbers = "1\n2,3";
+            var result = stringCalculatorService.Add(numbers);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void should_return_exception_when_string_contains_new_lines_and_comma_pasted()
+        {
+            var numbers = "1\n,2,3";
+            var result = stringCalculatorService.Add(numbers);
+        }
+
+        [TestMethod]
+        public void should_return_result_addition_when_user_change_delimiter()
+        {
+            var numbers = "//;\n1;2";
+            var result = stringCalculatorService.Add(numbers);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Negatives are not allowed (-3)")]
+        public void should_return_exception_when_string_contains_negatives_numbers()
+        {
+            var numbers = "1,2,-3";
+            var result = stringCalculatorService.Add(numbers);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Negatives are not allowed (-3,-4)")]
+        public void should_return_exception_when_string_contains_multiple_negatives_numbers()
+        {
+            var numbers = "1,2,-3,-4";
+            var result = stringCalculatorService.Add(numbers);
+        }
+    }
+}
